ed9914c9343302baf09bee6d1ac65041515cd805
Donny Marthen Sitompul <donnymarthen.sitompul@gmail.com>
2024-12-01 08:34:49 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 DATA_DIR = "data"  # Assuming data files are in a folder named "data"
 JUMLAH_DESA = "tapteng-2023-jumlah-desa.csv"
 LUAS_WILAYAH = "tapteng-2023-luas-kecamatan.csv"
+JENIS_KELAMIN = "tapteng-2023-jenis-kelamin-penduduk.csv"
 
 
 def load_data():
@@ -18,8 +19,11 @@ def load_data():
 
     jumlah_desa_df = pd.read_csv(f"{DATA_DIR}/{JUMLAH_DESA}",)
     luas_wilayah_df = pd.read_csv(f"{DATA_DIR}/{LUAS_WILAYAH}", )
+    jenis_kelamin_df = pd.read_csv(f"{DATA_DIR}/{JENIS_KELAMIN}", )
+
 
     merged_df = jumlah_desa_df.merge(luas_wilayah_df, on=["Kecamatan",])
+    merged_df = jenis_kelamin_df.merge(merged_df, on=["Kecamatan",])
 
     return merged_df
 
@@ -77,8 +81,31 @@ def plot_normal_distribution(df, col):
 if __name__ == "__main__":
     data = load_data()
     data = clean_data(data)
+    data["Jumlah Desa per km2 Wilayah"] = data["Jumlah Desa"]/data["Luas Wilayah"]
+    data["Luas Wilayah per Desa"] = data["Luas Wilayah"]/data["Jumlah Desa"]
+    data["Jumlah Laki-laki per Luas Wilayah per Desa"] = data["Laki-Laki"]/data["Luas Wilayah per Desa"]
+    data["Jumlah Perempuan per Luas Wilayah per Desa"] = data["Perempuan"]/data["Luas Wilayah per Desa"]
+    data["Ratio Perempuan per Laki-laki"] = data["Perempuan"]/data["Laki-Laki"]
+    
+
+
     print(data.columns)
 
-    plot_scatter(data, "Luas Wilayah", "Jumlah Desa")
-    plot_normal_distribution(data, "Luas Wilayah")
-    plot_normal_distribution(data, "Jumlah Desa")
+    #plot_scatter(data, "Luas Wilayah", "Jumlah Desa")
+    #plot_scatter(data, "Laki-Laki", "Jumlah Desa")
+    #plot_scatter(data, "Perempuan", "Jumlah Desa")
+    #plot_scatter(data, "Total", "Jumlah Desa")
+    #plot_scatter(data,"Ratio Perempuan per Laki-laki","Luas Wilayah")
+    #plot_scatter(data,"Ratio Perempuan per Laki-laki","Jumlah Desa")
+    #plot_scatter(data,"Ratio Perempuan per Laki-laki","Laki-Laki")
+    plot_scatter(data,"Laki-Laki","Ratio Perempuan per Laki-laki",)
+
+    #plot_normal_distribution(data, "Luas Wilayah")
+    #plot_normal_distribution(data, "Jumlah Desa")
+    #plot_normal_distribution(data, "Laki-Laki")
+    #plot_normal_distribution(data, "Perempuan")
+    #plot_normal_distribution(data, "Jumlah Desa per km2 Wilayah")
+    #plot_normal_distribution(data, "Luas Wilayah per Desa")
+    #plot_normal_distribution(data,"Jumlah Laki-laki per Luas Wilayah per Desa")
+    #plot_normal_distribution(data,"Jumlah Perempuan per Luas Wilayah per Desa")
+    #plot_normal_distribution(data,"Ratio Perempuan per Laki-laki")
